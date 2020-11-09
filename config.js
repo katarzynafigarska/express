6ab08a7e11fe89b3ebe4f8980b5c7b5360a61a3a
Kasia <figarskak@gmail.com>
2020-11-09 17:37:43 +0100

    admin login, admin news

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  db: '', //mongodb+srv://admin:E9Kg3HfkFFHgQmCf@cluster0.vrvuy.mongodb.net/<dbname>?retryWrites=true&w=majority
+  keySession: ['TWOJKLUCZ'],
+  maxAgeSession: 24 * 60 * 60 * 1000
+}
